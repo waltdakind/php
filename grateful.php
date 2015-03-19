bd37fe97e9f233bf558e26261e92921f7eea380c
Walter Mattingly <waltdakind@gmail.com>
2015-03-19 16:29:23 -0400

    Create grateful.php

---
+++ grateful.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    //just some fun with switch and case , along with some inside humore for deadheads
+    
+    //at some point switch the variable below into asking the user for input
+    $grateful = "Bruce";
+    switch($grateful) {
+        case("Jerry"): 
+            echo "Best guitar player in the Dead.";
+            break;
+        case("Bobby"):
+            echo "";
+             break;           
+        case("Donna"):
+            echo "Keep on caterwauling, Donna!";
+            break;
+        case("Mickey"):
+            echo "FAR OUT drums, dude!";
+            break;            
+        case("Bill"):
+            echo "FAR OUT drums, dude!";
+            break;            
+        case("Phil"):
+            echo "You're in the zone!";
+            break;            
+        case("Brent"):
+            echo "He could play those keys!";
+            break;            
+        case("Vince"):
+            echo "He could play those keys!";
+            break;            
+        case("Pigpen"):
+            echo "Always gonna miss Pigpen!";
+            break;            
+        case("Keith"):
+            echo "He did more than just marry Donna, really.";
+            break;            
+        case("Tom"):
+            echo "Yes, he was really in the band.";
+            break;            
+        case("TC"):
+            echo "Yes, he was really in the band.";
+              break;          
+        case("Billy"):
+            echo "FAR OUT drums, dude!";
+             break;  
+        case("Bruce"):
+            echo "Kinda, but not really in the band, dude!";
+             break;         
+        case("Branford"):
+            echo "Kinda, but not really in the band, dude!";
+             break; 
+        default:
+            echo "Wrong band, man!";
+            
+            }
+     
+    ?>
+	</body>
+</html>
